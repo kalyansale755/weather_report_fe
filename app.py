55b19d78cf1062fd15334f4cf0d4eaca4abd3c4e
kalyan <145825506+kalyansale755@users.noreply.github.com>
2026-06-08 15:58:43 +0530

    code changed

--- app.py
+++ app.py
@@ -1,18 +1,28 @@
 import streamlit as st
 import requests as rq
-backend_url=st.secrets["server_url"]
+
+backend_url = st.secrets["server_url"]
+
+st.write("Backend URL =", backend_url)
+
 st.title("AI_weather_forecasting")
+
 city = st.text_input("enter city")
 question = st.text_input("ask your question")
+
 if st.button("ask ai"):
-    res = rq.post(f"{backend_url}/get_weather",params={
-        "city":city,
-        "question":question
-    })
-    st.write("Backend URL:", backend_url)
-    
-    st.success(res.json()["messages"][-1]["content"])
+    try:
+        res = rq.post(
+            f"{backend_url}/get_weather",
+            params={
+                "city": city,
+                "question": question
+            },
+            timeout=60
+        )
 
-    
+        st.write("Status:", res.status_code)
+        st.json(res.json())
 
-    
+    except Exception as e:
+        st.error(repr(e))
