c146aca5c534cde988ff9857d0ba0998294f5310
kalyan <145825506+kalyansale755@users.noreply.github.com>
2026-06-08 12:12:10 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,14 @@
+import streamlit as st
+import requests as rq
+backend_url = "http://127.0.0.1:8000"
+st.title("AI_weather_forecasting")
+city = st.text_input("enter city")
+question = st.text_input("ask your question")
+if st.button("ask ai"):
+    res = rq.post(f"{backend_url}/get_weather",params={
+        "city":city,
+        "question":question
+    })
+    st.success(res.json()["messages"][-1]["content"])
+
+    
